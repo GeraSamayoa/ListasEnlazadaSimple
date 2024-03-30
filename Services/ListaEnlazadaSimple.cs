69ccfd08d0c0e636c285688e7d8ca6abadf93fcb
GeraSamayoa <gerasamayoa419@gmail.com>
2024-03-30 02:57:38 -0600

    Agregado Metodo Eliminar En una Posicion Especifica

--- Services/ListaEnlazadaSimple.cs
+++ Services/ListaEnlazadaSimple.cs
@@ -196,6 +196,60 @@ namespace Listas.Services
 			return $"Nodo agregado el nodo después de la posición: {posicion}.";
 		}
 
+		// Método para Eliminar un nodo en una posición específica
+		public string EliminarNodoEnPosicion(int posicion)
+		{
+			// Verificar si la lista está vacía
+			if (ListaVacia())
+			{
+				return "La lista está vacía";
+			}
+
+			// Verificar si la posición es inválida (menor o igual a cero)
+			if (posicion <= 0)
+			{
+				return "La posición especificada debe ser un número positivo y mayor que cero";
+			}
+
+			// Inicializar variables para recorrer la lista
+			Nodo? nodoActual = PrimerNodo;  // Nodo actual iniciado desde el primer nodo
+			Nodo? nodoAnterior = null;      // Nodo anterior inicializado como nulo, se usará para mantener la referencia al nodo anterior durante el recorrido
+			int index = 1;                 // Índice para mantener el seguimiento de la posición actual en la lista
+
+			// Recorrer la lista hasta encontrar la posición deseada o hasta el final de la lista.
+			while (nodoActual != null && index < posicion)
+			{
+				nodoAnterior = nodoActual;        // Mantener la referencia al nodo anterior
+				nodoActual = nodoActual.Referencia;  // Avanzar al siguiente nodo
+				index++;                          // Incrementar el índice
+			}
+
+			// Si  nodoActual == nulo , significa que la posición especificada está fuera de rango
+			if (nodoActual == null)
+			{
+				return "La posición especificada está fuera de rango";
+			}
+
+			// Si  nodoAnterior !=  nulo, significa que no estamos eliminando el primer nodo
+			if (nodoAnterior != null)
+			{
+				nodoAnterior.Referencia = nodoActual.Referencia;  // Eliminar el nodo actual actualizando las referencias del nodo anterior
+			}
+			else
+			{
+				// Si  nodoAnterior == nulo, significa que estamos eliminando el primer nodo de la lista
+				PrimerNodo = nodoActual.Referencia;  // Actualizar el primer nodo para que apunte al siguiente nodo
+			}
+
+			// Si  nodoActual es el último nodo de la lista, actualizar el último nodo
+			if (nodoActual == UltimoNodo)
+			{
+				UltimoNodo = nodoAnterior;  // El nodo anterior se convierte en el último nodo
+			}
+
+			return "¡Nodo eliminado correctamente!";
+		}
+
 		//Metodo para buscar un nodo por su valor en una lista enlazada simple desordenada
 		public Nodo? BuscarNodoPorValor(string elemento)
 		{
