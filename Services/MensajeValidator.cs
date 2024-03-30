7b92251f94752cb168640260d096ed8dd01134c6
GeraSamayoa <gerasamayoa419@gmail.com>
2024-03-30 03:47:15 -0600

    Clase MensajeValidator funcionando

---
+++ Services/MensajeValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace Listas.Services
+{
+	public class MensajeValidator
+	{
+		
+
+		public (string? mensajeError,string valor, int? posicion) MensajeInsertar(string mensaje, string? mensajeError, int? posicion, string valor)
+		{
+			
+			if (mensaje.StartsWith("Nodo agregado"))
+			{
+				return (null, string.Empty, null);
+			}
+			else
+			{
+				return (mensaje, valor, posicion);
+			}
+		}
+	}
+}
