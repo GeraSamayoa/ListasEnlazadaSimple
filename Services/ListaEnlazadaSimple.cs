6440723cf03656863c862f9221ed5023e44da0ae
JHOAN ALEXANDER MORAN BELTRAN <jmoranb1@miumg.edu.gt>
2024-03-31 00:28:13 -0600

    si pude

--- Services/ListaEnlazadaSimple.cs
+++ Services/ListaEnlazadaSimple.cs
@@ -67,5 +67,40 @@ namespace Listas.Services
 			}
 
 		}
+
+		public string EliminarDespuesDe(int posicion)
+		{
+			if (ListaVacia())
+			{
+				// La lista está vacía o la posición es inválida.
+				return "la lista esta vacia";
+			}
+
+			if (posicion <= 0)
+			{
+				// La lista está vacía o la posición es inválida.
+				return "la Pocision es invalida, ingrese un numero mayor del 0";
+			}
+			// Se inicia en la cabeza y se recorre la lista hasta la posición especificada.
+			Nodo? nodoActual = PrimerNodo;
+			for (int i = 1; i < posicion && nodoActual != null; i++)
+			{
+				// Avanza hasta el nodo en la posición especificada.
+				nodoActual = nodoActual.Referencia;
+			}
+            if (nodoActual?.Referencia == null || nodoActual == null)
+            {
+				return "Posicion fuera de rango";
+            }
+
+            // Si hay un nodo después del actual, se elimina cambiando la referencia Siguiente.
+            if (nodoActual?.Referencia != null)
+			{
+				// El nodo a eliminar es el siguiente del nodo actual.
+				nodoActual.Referencia = nodoActual.Referencia.Referencia;
+			}
+
+			return "!Nodo eliminado Exitosamente";
+		}
 	}
 }
